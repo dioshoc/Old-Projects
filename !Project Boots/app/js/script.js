0b8207feae96c7e0a969493b349249adbb852579
COMPUTER\е4 аа <dioshoc@gmail.com>
2021-01-17 16:23:51 +0300

    transfer to portfolio

--- !Project Boots/app/js/script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    /* Start Welcome Section */
-    $("#scroll_bottom").click(function () {
-        var href = $(this).attr("href");
-        $("html, body").animate({ scrollTop: $(href).offset().top + "px" });
-        return false;
-    });
-    /* End Welcome Section */
-});
